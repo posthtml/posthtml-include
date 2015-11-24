ed281fd2cf2b5031e379df9656aab270297b81de
Ivan Voischev <voischev.ivan@ya.ru>
2015-11-24 12:49:46 +0300

    fix plugin name

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var parser = require('posthtml-parser');
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function posthtmlInclude(options) {
+module.exports = function (options) {
     options = options || {};
     options.root = options.root || './';
     options.encoding = options.encoding || 'utf-8';
 
-    return function(tree) {
+    return function posthtmlInclude(tree) {
         tree.match({ tag: 'include' }, function(node) {
             var src = node.attrs.src || false,
                 content;
