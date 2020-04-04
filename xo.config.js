a8f1fe4446aa52b3abdb2dd00c14d9326bf68c19
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-04 17:47:38 +0300

    refactor: code refresh

---
+++ xo.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  space: true,
+  rules: {
+    semi: 0,
+    'unicorn/string-content': 0,
+    'ava/prefer-async-await': 0,
+    'promise/prefer-await-to-then': 0,
+    'unicorn/no-abusive-eslint-disable': 0,
+    quotes: ['error', 'single', {allowTemplateLiterals: true}]
+  }
+};
